9af35f69d8d6ac991ac1be3098cfed56b6d92fae
maribel00 <maria.ruizmartinez@epfl.ch>
2023-08-07 11:03:16 +0200

    Chapter 11 modified.

--- code/R/Statistical analyses/correlation.R
+++ code/R/Statistical analyses/correlation.R
@@ -1,15 +1,19 @@
 setwd('./Escritorio/5º DGIIM/TFG/Analysis-of-processes/code/R/')
 
-source("LCV_Theme.R")
-source("LCV_Regression.R")
-source("SIIE2023.R")
+source("SIIE23Scripts/SIIE23master.R")
+library(patchwork)
 
-data <- doLoadData()
-head(data)
+# data <- readBaseDataset()
+# data <- data[data$Level == 10,]
+
+data <- SIIE23doLoadData()
+
+data <- data[data$s < 1000,]
+data <- data[data$p > 6,]
+nrow(data)
 
 LCV_Heaven_Theme()
 
-library(patchwork)
 nfields <- c("np","ns","ot","rt","st","ft","fr","ps","sq")
 LCV_Mosaic <- LCV_LinearRegression(data,x=nfields[1],y="Grade",full=TRUE)
 for (field in nfields[2:length(nfields)]){
@@ -18,3 +22,40 @@ for (field in nfields[2:length(nfields)]){
 }
 LCV_Mosaic + plot_layout(nc=3)
 
+LCV_Mosaic <- LCV_PolynomialRegression(data,x=nfields[1],y="Grade",full=TRUE)
+for (field in nfields[2:length(nfields)]){
+  lplot <- LCV_PolynomialRegression(data,x=field,y="Grade",full=TRUE)
+  LCV_Mosaic <- LCV_Mosaic + lplot
+}
+LCV_Mosaic + plot_layout(nc=3)
+
+LCV_Mosaic <- LCV_ExponentialRegression(data,x=nfields[1],y="Grade",full=TRUE)
+for (field in nfields[2:length(nfields)]){
+  lplot <- LCV_ExponentialRegression(data,x=field,y="Grade",full=TRUE)
+  LCV_Mosaic <- LCV_Mosaic + lplot
+}
+LCV_Mosaic + plot_layout(nc=3)
+
+data <- readBaseDataset()
+data <- data[data$Level == 10,]
+
+LCV_Mosaic <- LCV_LinearRegression(data,x=topologicalmetrics()[1],y="Grade",full=TRUE)
+for (field in topologicalmetrics()[2:length(topologicalmetrics())]){
+  lplot <- LCV_LinearRegression(data,x=field,y="Grade",full=TRUE)
+  LCV_Mosaic <- LCV_Mosaic + lplot
+}
+LCV_Mosaic + plot_layout(nc=3)
+
+LCV_Mosaic <- LCV_PolynomialRegression(data,x=topologicalmetrics()[1],y="Grade",full=TRUE)
+for (field in topologicalmetrics()[2:length(topologicalmetrics())]){
+  lplot <- LCV_PolynomialRegression(data,x=field,y="Grade",full=TRUE)
+  LCV_Mosaic <- LCV_Mosaic + lplot
+}
+LCV_Mosaic + plot_layout(nc=3)
+
+LCV_Mosaic <- LCV_ExponentialRegression(data,x=topologicalmetrics()[1],y="Grade",full=TRUE)
+for (field in topologicalmetrics()[2:length(topologicalmetrics())]){
+  lplot <- LCV_ExponentialRegression(data,x=field,y="Grade",full=TRUE)
+  LCV_Mosaic <- LCV_Mosaic + lplot
+}
+LCV_Mosaic + plot_layout(nc=3)
---
+++ code/R/Statistical analyses/regressionst.R
@@ -0,0 +1,27 @@
+setwd('./Escritorio/5º DGIIM/TFG/Analysis-of-processes/code/R/')
+
+source("SIIE23Scripts/SIIE23master.R")
+
+# data <- doLoadData()
+# data <- data[data$s < 1000,]
+# data <- data[data$p > 6,]
+# nrow(data)
+
+data <- readBaseDataset()
+data <- data[data$Level == 10,]
+head(data)
+nrow(data)
+
+LCV_Orange_Theme()
+k <- LCV_ClusterColumn(data,"St")
+k
+
+LCV_ClusterColumn(data,"St",nc=5)
+# Partition:  0 4.276666 7.860185 11.48007 28.98446  Accuracy fit$betweenss/fit$totss=  0.9673813
+
+LCV_Heaven_Theme()
+LCV_density(data, "St", showall = TRUE)
+
+LCV_Heaven_Theme()
+datawo <- data[data$St < 28,]
+LCV_LinearRegression(datawo,x="St",y="Grade",full=TRUE)
